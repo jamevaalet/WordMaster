d53d0754af629296704c98f6ee495a4016edc352
Jame Vaalet <jamevaa@microsoft.com>
2016-01-08 11:01:04 +0000

    first commit

---
+++ WordMaster/Models/Word.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WordMaster.Models
+{
+	public class Word
+	{
+		public int WordID { get; set; }
+		public string Name { get; set; }
+		public string Definition { get; set; }
+		public List<WordUsage> WordUsages { get; set; }
+
+	}
+}
---
+++ WordMaster/Models/WordUsage.cs
@@ -0,0 +1,8 @@
+﻿namespace WordMaster.Models
+{
+	public class WordUsage
+	{
+		public int WordID { get; set; }
+		public string Usage { get; set; }
+	}
+}
